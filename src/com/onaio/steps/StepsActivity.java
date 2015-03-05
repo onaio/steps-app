32da7dc4c778af5f63350f17ccc199cbc4269507
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2015-03-05 18:58:00 +0530

    wire up add household action

--- src/com/onaio/steps/StepsActivity.java
+++ src/com/onaio/steps/StepsActivity.java
@@ -2,21 +2,19 @@ package com.onaio.steps;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
-import java.util.List;
-
-import static java.util.Arrays.asList;
+import java.util.ArrayList;
 
 public class StepsActivity extends ListActivity {
 
     public static final String PHONE_ID = "phoneId";
     private static final int IDENTIFIER = 1;
-    private static final List<String> HOUSEHOLDS = asList("Household-1", "Household-2");
     private String phoneId;
 
     @Override
@@ -24,7 +22,7 @@ public class StepsActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         phoneId = fetchPhoneId();
-        getListView().setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, HOUSEHOLDS));
+        getListView().setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fetchHouseholds()));
     }
 
     @Override
@@ -39,6 +37,9 @@ public class StepsActivity extends ListActivity {
         switch (item.getItemId()) {
             case R.id.action_settings:
                 openSettings();
+                return true;
+            case R.id.action_add:
+                addHousehold();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -59,12 +60,35 @@ public class StepsActivity extends ListActivity {
         startActivityForResult(intent, IDENTIFIER);
     }
 
+    private void addHousehold() {
+        ArrayAdapter<String> adapter = (ArrayAdapter<String>) getListView().getAdapter();
+        adapter.add(String.format("%s-%d", phoneId, adapter.getCount() + 1));
+    }
+
     private String fetchPhoneId() {
-        return null;
+        return dataStore().getString(PHONE_ID, null);
+    }
+
+    private ArrayList<String> fetchHouseholds() {
+        return new ArrayList<String>();
     }
 
     private String savePhoneId(String phoneId) {
+        SharedPreferences.Editor editor = dataStoreEditor();
+        editor.putString(PHONE_ID, phoneId);
+        if (editor.commit()) {
+            return phoneId;
+        }
+        savePhoneIdErrorHandler();
         return null;
+    }
+
+    private SharedPreferences.Editor dataStoreEditor() {
+        return dataStore().edit();
+    }
+
+    private SharedPreferences dataStore() {
+        return getPreferences(MODE_PRIVATE);
     }
 
     private void savePhoneIdErrorHandler() {
