f761efd8709ef5184edf420a6ea9b0e892eab589
Gaurav Singhania <gauravs@sahajsoft.com>
2015-03-12 10:53:08 +0530

    Populating generated id on user input | <S>

--- src/main/java/com/onaio/steps/activity/NewHouseholdActivity.java
+++ src/main/java/com/onaio/steps/activity/NewHouseholdActivity.java
@@ -3,9 +3,10 @@ package com.onaio.steps.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.TextView;
-
 import com.onaio.steps.R;
 import com.onaio.steps.helper.DatabaseHelper;
 import com.onaio.steps.model.Household;
@@ -13,15 +14,43 @@ import com.onaio.steps.model.Household;
 public class NewHouseholdActivity extends Activity {
 
     public static final String HOUSEHOLD_NAME = "household_name";
+    private String phoneId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_household);
+        populatePhoneIdInGeneratedId();
+        bindGeneratedIdToTextInput();
+
+    }
+
+    private void bindGeneratedIdToTextInput() {
+        TextView householdName = (TextView) findViewById(R.id.household_name);
+        householdName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                TextView phoneIdView = (TextView) findViewById(R.id.generated_household_id);
+                phoneIdView.setText(phoneId +"-"+editable.toString().trim());
+            }
+        });
+    }
+
+    private void populatePhoneIdInGeneratedId() {
         Intent intent = getIntent();
-        String phoneId = intent.getStringExtra(StepsActivity.PHONE_ID);
+        phoneId = intent.getStringExtra(StepsActivity.PHONE_ID);
         TextView phoneIdView = (TextView) findViewById(R.id.generated_household_id);
-        phoneIdView.setText(phoneId+"-");
+        phoneIdView.setText(phoneId +"-");
     }
 
     public void saveHousehold(View view) {
