e12754ecff37e78ae114f8db35dce2ed361b8fe2
Gaurav Singhania <gauravs@sahajsoft.com>
2015-03-13 14:52:14 +0530

    Added menu for member activity | <S>

--- src/main/java/com/onaio/steps/activity/MemberActivity.java
+++ src/main/java/com/onaio/steps/activity/MemberActivity.java
@@ -3,6 +3,8 @@ package com.onaio.steps.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,10 +16,32 @@ import com.onaio.steps.model.Household;
 import com.onaio.steps.model.Member;
 
 public class MemberActivity extends Activity {
-   @Override
+
+    private Member member;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member);
+        populateMember();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.member_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void populateMember() {
+        Intent intent = getIntent();
+        member = (Member) intent.getSerializableExtra(Constants.MEMBER);
+        TextView nameView = (TextView) findViewById(R.id.member_name);
+        TextView ageView = (TextView) findViewById(R.id.member_age);
+        TextView genderView = (TextView) findViewById(R.id.member_gender);
+        nameView.setText(String.valueOf(member.getName()));
+        ageView.setText(String.valueOf(member.getAge()));
+        genderView.setText(String.valueOf(member.getGender()));
     }
 
 }
