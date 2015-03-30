24ab84840c4b67cb713e27ebeefd77558fd5eb80
Manisha Rana <manishar@sahajsoft.com>
2015-03-30 18:12:07 +0530

    Refactoring MemberViewWrapperTest and another TestCase added <M>

--- src/test/java/com/onaio/steps/modelViewWrapper/MemberViewWrapperTest.java
+++ src/test/java/com/onaio/steps/modelViewWrapper/MemberViewWrapperTest.java
@@ -38,92 +38,81 @@ public class MemberViewWrapperTest {
     public void Setup(){
         newMemberActivity = Robolectric.setupActivity(NewMemberActivity.class);
         memberViewWrapper = new MemberViewWrapper(newMemberActivity);
-
     }
 
     @Test
     public void ShouldRaiseExceptionWhenSurnameIsInvalid() throws InvalidDataException {
         String errorString = Constants.ERROR_STRING;
-
         TextView surname = (TextView) newMemberActivity.findViewById(R.id.member_family_surname);
-        TextView firstname = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
+        TextView firstName = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
         RadioGroup gender = (RadioGroup) newMemberActivity.findViewById(R.id.member_gender);
         TextView age = (TextView)newMemberActivity.findViewById(R.id.member_age);
+        surname.setText("");
+        firstName.setText("Manisha");
+        gender.check(R.id.female_selection);
+        age.setText("23");
+        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
 
         expectedException.expect(InvalidDataException.class);
         expectedException.expectMessage(String.format(errorString,"Member",Constants.FAMILY_SURNAME));
 
-        surname.setText("");
-        firstname.setText("Manisha");
-        gender.check(R.id.female_selection);
-        age.setText("23");
-
-        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
-        memberViewWrapper.getMember(surname.getId(),firstname.getId(),gender.getId(), age.getId(),household);
+        memberViewWrapper.getMember(surname.getId(),firstName.getId(),gender.getId(), age.getId(),household);
     }
 
     @Test
-    public void ShouldRaiseExceptionWhenFirstnameIsInvalid() throws InvalidDataException {
+    public void ShouldRaiseExceptionWhenFirstNameIsInvalid() throws InvalidDataException {
         String errorString = Constants.ERROR_STRING;
-
         TextView surname = (TextView) newMemberActivity.findViewById(R.id.member_family_surname);
-        TextView firstname = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
+        TextView firstName = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
         RadioGroup gender = (RadioGroup) newMemberActivity.findViewById(R.id.member_gender);
         TextView age = (TextView)newMemberActivity.findViewById(R.id.member_age);
+        surname.setText("Rana");
+        firstName.setText("");
+        gender.check(R.id.female_selection);
+        age.setText("23");
+        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
 
         expectedException.expect(InvalidDataException.class);
         expectedException.expectMessage(String.format(errorString,"Member",Constants.FIRST_NAME));
 
-        surname.setText("Rana");
-        firstname.setText("");
-        gender.check(R.id.female_selection);
-        age.setText("23");
-
-        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
-        memberViewWrapper.getMember(surname.getId(),firstname.getId(),gender.getId(), age.getId(),household);
+        memberViewWrapper.getMember(surname.getId(),firstName.getId(),gender.getId(), age.getId(),household);
     }
 
     @Test
     public void ShouldRaiseExceptionWhenGenderIsInvalid() throws InvalidDataException {
-
         String errorString = Constants.ERROR_STRING;
-
         TextView surname = (TextView) newMemberActivity.findViewById(R.id.member_family_surname);
-        TextView firstname = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
+        TextView firstName = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
         RadioGroup gender = (RadioGroup) newMemberActivity.findViewById(R.id.member_gender);
         TextView age = (TextView)newMemberActivity.findViewById(R.id.member_age);
+        surname.setText("Rana");
+        firstName.setText("Manisha");
+        age.setText("23");
+        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
 
         expectedException.expect(InvalidDataException.class);
         expectedException.expectMessage(String.format(errorString,"Member",Constants.GENDER));
 
-        surname.setText("Rana");
-        firstname.setText("Manisha");
-        age.setText("23");
-
-        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
-        memberViewWrapper.getMember(surname.getId(),firstname.getId(),gender.getId(), age.getId(),household);
+        memberViewWrapper.getMember(surname.getId(),firstName.getId(),gender.getId(), age.getId(),household);
     }
 
 
     @Test
     public void ShouldRaiseExceptionWhenAgeIsInvalid() throws InvalidDataException {
-
         String errorString = Constants.ERROR_STRING;
-
         TextView surname = (TextView) newMemberActivity.findViewById(R.id.member_family_surname);
-        TextView firstname = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
+        TextView firstName = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
         RadioGroup gender = (RadioGroup) newMemberActivity.findViewById(R.id.member_gender);
         TextView age = (TextView)newMemberActivity.findViewById(R.id.member_age);
+        surname.setText("Rana");
+        firstName.setText("Manisha");
+        gender.check(R.id.female_selection);
+        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
 
         expectedException.expect(InvalidDataException.class);
         expectedException.expectMessage(String.format(errorString,"Member",Constants.AGE));
 
-        surname.setText("Rana");
-        firstname.setText("Manisha");
-        gender.check(R.id.female_selection);
-
-        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
-        memberViewWrapper.getMember(surname.getId(),firstname.getId(),gender.getId(), age.getId(),household);
+        memberViewWrapper.getMember(surname.getId(),firstName.getId(),gender.getId(), age.getId(),household);
     }
 
     @Test
@@ -132,19 +121,33 @@ public class MemberViewWrapperTest {
         TextView firstname = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
         RadioGroup gender = (RadioGroup) newMemberActivity.findViewById(R.id.member_gender);
         TextView age = (TextView)newMemberActivity.findViewById(R.id.member_age);
-
         surname.setText("Bansal");
         firstname.setText("Rohit");
         gender.check(R.id.male_selection);
         age.setText("12");
-
         Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
+
         Member member = memberViewWrapper.getMember(surname.getId(), firstname.getId(), gender.getId(), age.getId(), household);
+
         Assert.assertTrue(member.getFamilySurname().equals("Bansal"));
         Assert.assertTrue(member.getFirstName().equals("Rohit"));
         Assert.assertTrue(member.getGender().equals("Male"));
         Assert.assertTrue(member.getAge()==12);
+    }
 
+    @Test
+    public void ShouldRaiseExceptionWhenMultipleFieldsAreEmpty() throws InvalidDataException {
+        String errorString = Constants.ERROR_STRING;
+        TextView surname = (TextView) newMemberActivity.findViewById(R.id.member_family_surname);
+        TextView firstName = (TextView) newMemberActivity.findViewById(R.id.member_first_name);
+        RadioGroup gender = (RadioGroup) newMemberActivity.findViewById(R.id.member_gender);
+        TextView age = (TextView)newMemberActivity.findViewById(R.id.member_age);
+        Household household = new Household("1","Any Household", "123456789","", HouseholdStatus.NOT_SELECTED, new Date().toString());
+
+        expectedException.expect(InvalidDataException.class);
+        expectedException.expectMessage(String.format(errorString,"Member",Constants.FAMILY_SURNAME,Constants.FIRST_NAME,Constants.GENDER,Constants.AGE));
+
+        memberViewWrapper.getMember(surname.getId(), firstName.getId(), gender.getId(), age.getId(), household);
 
     }
 
