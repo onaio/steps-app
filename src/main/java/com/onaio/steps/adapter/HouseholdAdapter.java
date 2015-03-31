ed21bf8bab8a92e58ba35e89578853b3c96ec9fb
Gaurav Singhania <gauravs@sahajsoft.com>
2015-03-31 13:58:23 +0530

    UI : Changing the UI of the household list | <S>

--- src/main/java/com/onaio/steps/adapter/HouseholdAdapter.java
+++ src/main/java/com/onaio/steps/adapter/HouseholdAdapter.java
@@ -6,9 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.TwoLineListItem;
 
+import com.onaio.steps.R;
 import com.onaio.steps.helper.DatabaseHelper;
 import com.onaio.steps.model.Household;
 import com.onaio.steps.model.Member;
@@ -46,7 +47,7 @@ public class HouseholdAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TwoLineListItem householdItemView;
+        View householdItemView;
         Household householdAtPosition = households.get(position);
 
         householdItemView = getViewItem(convertView);
@@ -56,24 +57,32 @@ public class HouseholdAdapter extends BaseAdapter{
     }
 
 
-    private void setTextInView(TwoLineListItem twoLineListItem, Household householdAtPosition) {
-        TextView text1 = twoLineListItem.getText1();
-        TextView text2 = twoLineListItem.getText2();
-        text1.setTextColor(Color.BLACK);
-        text1.setText(householdAtPosition.getName());
+    private void setTextInView(View householdListItem, Household householdAtPosition) {
+        TextView householdName = (TextView) householdListItem.findViewById(R.id.main_text);
+        TextView membersCount = (TextView) householdListItem.findViewById(R.id.sub_text);
+        ImageView image = (ImageView) householdListItem.findViewById(R.id.main_image);
+        image.setImageResource(getImage(householdAtPosition));
+        householdName.setTextColor(Color.BLACK);
+        householdName.setText(householdAtPosition.getName());
         int numberOfMembers = Member.numberOfNonDeletedMembers(new DatabaseHelper(context), householdAtPosition);
-        text2.setText(String.format("%s, %d members", householdAtPosition.getCreatedAt(),numberOfMembers));
+        membersCount.setText(String.format("%s, %d members", householdAtPosition.getCreatedAt(), numberOfMembers));
     }
 
-    private TwoLineListItem getViewItem(View convertView) {
-        TwoLineListItem twoLineListItem;
+    private int getImage(Household householdAtPosition) {
+        switch (householdAtPosition.getStatus()){
+            case DONE: return R.drawable.ic_survey_done;
+            default: return R.drawable.ic_survey_not_done;
+        }
+    }
+
+    private View getViewItem(View convertView) {
+        View view;
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            twoLineListItem = (TwoLineListItem) inflater.inflate(
-                    android.R.layout.simple_list_item_2, null);
+            view = inflater.inflate(R.layout.list_item, null);
         } else
-            twoLineListItem = (TwoLineListItem) convertView;
-        return twoLineListItem;
+            view = convertView;
+        return view;
     }
 }
