8717785b21ca563c1e2a0af5f25f84b01c657537
Manisha Rana <manishar@sahajsoft.com>
2015-05-20 10:18:06 +0530

    NewParticipantActivity() added- User should be able to add participant from the View<M>

--- src/main/java/com/onaio/steps/activity/NewParticipantActivity.java
+++ src/main/java/com/onaio/steps/activity/NewParticipantActivity.java
@@ -1,7 +1,55 @@
 package com.onaio.steps.activity;
 
-/**
- * Created by manisharana on 5/20/15.
- */
-public class NewParticipantActivity {
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.onaio.steps.R;
+import com.onaio.steps.exception.InvalidDataException;
+import com.onaio.steps.helper.Constants;
+import com.onaio.steps.helper.CustomDialog;
+import com.onaio.steps.helper.DatabaseHelper;
+import com.onaio.steps.model.Participant;
+import com.onaio.steps.modelViewWrapper.ParticipantViewWrapper;
+
+public class NewParticipantActivity  extends Activity{
+
+    private final DatabaseHelper db = new DatabaseHelper(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        populateView();
+    }
+
+    private void populateView() {
+        setContentView(R.layout.participant_form);
+        TextView header = (TextView) findViewById(R.id.form_header);
+        header.setText(R.string.action_add_participant);
+        Button doneButton = (Button) findViewById(R.id.ic_done);
+        doneButton.setText(R.string.add);
+    }
+
+    public void save(View view) {
+        Intent intent =new Intent();
+        try{
+
+            Participant participant = new ParticipantViewWrapper(this)
+                    .getParticipant(R.id.participant_id_value ,R.id.member_family_surname, R.id.member_first_name, R.id.member_gender, R.id.member_age);
+            participant.save(db);
+            intent.putExtra(Constants.PARTICIPANT,participant);
+            setResult(RESULT_OK, intent);
+            finish();
+        } catch (InvalidDataException e) {
+            new CustomDialog().notify(this, CustomDialog.EmptyListener,e.getMessage(),R.string.error_title);
+        }
+    }
+
+    public void cancel(View view){
+        finish();
+    }
 }
