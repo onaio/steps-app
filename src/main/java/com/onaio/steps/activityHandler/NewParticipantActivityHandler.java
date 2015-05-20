d1653a4b6907764fbda4283417b60ffb6325fec5
Manisha Rana <manishar@sahajsoft.com>
2015-05-20 10:28:46 +0530

    NewParticipantActivityHandler and ParticipantActivityHandler added<M>

--- src/main/java/com/onaio/steps/activityHandler/NewParticipantActivityHandler.java
+++ src/main/java/com/onaio/steps/activityHandler/NewParticipantActivityHandler.java
@@ -1,7 +1,33 @@
 package com.onaio.steps.activityHandler;
 
-/**
- * Created by manisharana on 5/19/15.
- */
-public class NewParticipantActivityHandler {
+import android.app.ListActivity;
+import android.content.Intent;
+
+import com.onaio.steps.activityHandler.Interface.IActivityResultHandler;
+import com.onaio.steps.activityHandler.Interface.IMenuHandler;
+
+public class NewParticipantActivityHandler implements IActivityResultHandler,IMenuHandler {
+    public NewParticipantActivityHandler(ListActivity activity) {
+    }
+
+
+    @Override
+    public void handleResult(Intent data, int resultCode) {
+
+    }
+
+    @Override
+    public boolean canHandleResult(int requestCode) {
+        return false;
+    }
+
+    @Override
+    public boolean shouldOpen(int menu_id) {
+        return false;
+    }
+
+    @Override
+    public boolean open() {
+        return false;
+    }
 }
--- src/main/java/com/onaio/steps/activityHandler/ParticipantActivityHandler.java
+++ src/main/java/com/onaio/steps/activityHandler/ParticipantActivityHandler.java
@@ -1,7 +1,19 @@
 package com.onaio.steps.activityHandler;
 
+import android.app.ListActivity;
+
+import com.onaio.steps.activityHandler.Interface.IListItemHandler;
+import com.onaio.steps.model.Participant;
+
 /**
  * Created by manisharana on 5/20/15.
  */
-public class ParticipantActivityHandler {
+public class ParticipantActivityHandler implements IListItemHandler {
+    public ParticipantActivityHandler(ListActivity activity, Participant participant) {
+    }
+
+    @Override
+    public boolean open() {
+        return false;
+    }
 }
