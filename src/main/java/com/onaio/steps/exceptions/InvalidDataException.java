65d9a069caa8f0a6ba65a7b9965cf5514d203e46
James Maina <jmaina@ona.io>
2016-03-30 10:40:36 +0300

    More fixes to  Update Spanish/French translations in the app #29 issue

--- src/main/java/com/onaio/steps/exceptions/InvalidDataException.java
+++ src/main/java/com/onaio/steps/exceptions/InvalidDataException.java
@@ -11,8 +11,13 @@ import java.util.List;
 public class InvalidDataException extends Exception {
 
     public InvalidDataException(Activity activity, String modelName, List<String> errorFields) {
-        super(String.format(activity.getString(R.string.invalid)+" %s, " +activity.getString(R.string.fill_correct_message)+" %s.",modelName, StringUtils.join(errorFields.toArray(),',')));
+       super((activity.getString(R.string.action_settings).equalsIgnoreCase(modelName))?
+               activity.getString(R.string.invalid_settings)+" "+StringUtils.join(errorFields.toArray(), ','):(activity.getString(R.string.action_member).equalsIgnoreCase(modelName))?
+               activity.getString(R.string.invalid_member)+" "+StringUtils.join(errorFields.toArray(),','):"");
+
+        //super(String.format(activity.getString(R.string.invalid)+" %s, " +activity.getString(R.string.fill_correct_message)+" %s.",modelName, StringUtils.join(errorFields.toArray(),',')));
     }
+
 
     public InvalidDataException(List<String> errorMessages) {
         super(StringUtils.join(errorMessages.toArray(),','));
