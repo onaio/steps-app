d0eebeaa8cd6990eae38b68208c9c5edf48b68d2
Jason Rogena <jasonrogena@gmail.com>
2016-10-13 15:52:43 +0300

    Wipe Data Button
    Add the 'Wipe Data' button in the settings screen
    
    Part of fix for issue #35
    
    Signed-off-by: Jason Rogena <jasonrogena@gmail.com>

--- src/main/java/com/onaio/steps/activities/SettingsActivity.java
+++ src/main/java/com/onaio/steps/activities/SettingsActivity.java
@@ -16,16 +16,10 @@
 
 package com.onaio.steps.activities;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.provider.DocumentFile;
 import android.view.View;
 
 import android.widget.TextView;
@@ -34,23 +28,22 @@ import android.widget.Toast;
 import com.onaio.steps.exceptions.InvalidDataException;
 import com.onaio.steps.handler.actions.ImportHandler;
 import com.onaio.steps.helper.CustomDialog;
+import com.onaio.steps.helper.DatabaseHelper;
 import com.onaio.steps.orchestrators.FlowOrchestrator;
 import com.onaio.steps.orchestrators.flows.FlowType;
 import com.onaio.steps.R;
 import com.onaio.steps.helper.Constants;
 
-import java.io.OutputStream;
-
-import static android.app.PendingIntent.getActivity;
-
 public class SettingsActivity extends Activity {
 
     private FlowType flowType;
     private FlowOrchestrator flowOrchestrator;
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = new DatabaseHelper(this);
         Intent intent = getIntent();
         flowType = FlowType.valueOf(intent.getStringExtra(Constants.FLOW_TYPE));
         flowOrchestrator = new FlowOrchestrator(this);
@@ -104,6 +97,17 @@ public class SettingsActivity extends Activity {
             }
         };
         new CustomDialog().confirm(this, confirmListener, CustomDialog.EmptyListener, R.string.warning_merging_data, R.string.warning_title);
+    }
+
+    public void eraseData(View view) {
+        DialogInterface.OnClickListener confirmListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                db.truncate();
+                Toast.makeText(SettingsActivity.this, R.string.data_wipe_successful, Toast.LENGTH_LONG).show();
+            }
+        };
+        new CustomDialog().confirm(this, confirmListener, CustomDialog.EmptyListener, R.string.warning_erasing_data, R.string.warning_title);
     }
 
 }
