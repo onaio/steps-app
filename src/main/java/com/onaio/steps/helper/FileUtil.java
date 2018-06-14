fa3791e7b371e5e2ae8dedd3d3345c69acfd0043
Ephraim Kigamba <ekigamba@ona.io>
2018-06-14 13:04:24 +0300

    Add QR Code en/decode & file im/export utils

--- src/main/java/com/onaio/steps/helper/FileUtil.java
+++ src/main/java/com/onaio/steps/helper/FileUtil.java
@@ -16,6 +16,10 @@
 
 package com.onaio.steps.helper;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
 import com.opencsv.CSVParser;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
@@ -23,18 +27,35 @@ import com.opencsv.CSVWriter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class FileUtil {
+
+    private static final String TAG = FileUtil.class.getName();
 
     private String[] headers;
     private List<String[]> data = new ArrayList<String[]>();
     private Exception exception;
+
+    // Used to validate and display valid form names.
+    public static final String VALID_FILENAME = "[ _\\-A-Za-z0-9]*.x[ht]*ml";
+    public static final String FORMID = "formid";
+    public static final String VERSION = "version"; // arbitrary string in OpenRosa 1.0
+    public static final String TITLE = "title";
+    public static final String SUBMISSIONURI = "submission";
+    public static final String BASE64_RSA_PUBLIC_KEY = "base64RsaPublicKey";
+    public static final String AUTO_DELETE = "autoDelete";
+    public static final String AUTO_SEND = "autoSend";
+    static int bufSize = 16 * 1024; // May be set by unit test
 
     public FileUtil withHeader(String[] headers){
         this.headers = headers;
@@ -88,6 +109,67 @@ public class FileUtil {
         fileWriter.close();
         printStream.flush();
         printStream.close();
+    }
+
+    @SuppressWarnings("NewApi")
+    public static void saveBitmapToFile(Bitmap bitmap, String path) {
+        final Bitmap.CompressFormat compressFormat = path.toLowerCase(Locale.getDefault()).endsWith(".png") ?
+                Bitmap.CompressFormat.PNG : Bitmap.CompressFormat.JPEG;
+
+        try (FileOutputStream out = new FileOutputStream(path)) {
+            bitmap.compress(compressFormat, 100, out);
+        } catch (Exception e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    /*
+    This method is used to avoid OutOfMemoryError exception during loading an image.
+    If the exception occurs we catch it and try to load a smaller image.
+     */
+    public static Bitmap getBitmap(String path, BitmapFactory.Options originalOptions) {
+        BitmapFactory.Options newOptions = new BitmapFactory.Options();
+        newOptions.inSampleSize = originalOptions.inSampleSize;
+        if (newOptions.inSampleSize <= 0) {
+            newOptions.inSampleSize = 1;
+        }
+        Bitmap bitmap;
+        try {
+            bitmap = BitmapFactory.decodeFile(path, originalOptions);
+        } catch (OutOfMemoryError e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+            newOptions.inSampleSize++;
+            return getBitmap(path, newOptions);
+        }
+
+        return bitmap;
+    }
+
+    public static byte[] read(File file) {
+        byte[] bytes = {};
+        try {
+            bytes = new byte[(int) file.length()];
+            InputStream is = new FileInputStream(file);
+            is.read(bytes);
+            is.close();
+        } catch (IOException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+        return bytes;
+    }
+
+    @SuppressWarnings("NewApi")
+    public static void write(File file, byte[] data) {
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(data);
+            fos.close();
+        } catch (IOException e) {
+            Log.e(TAG, Log.getStackTraceString(e));
+        }
+    }
+
+    private static void logInfo(String format, double toReplace) {
+        Log.i(TAG, String.format(format, toReplace));
     }
 
 }
