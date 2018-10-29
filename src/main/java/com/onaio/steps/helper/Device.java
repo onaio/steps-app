32c83b9e05a1872b53f82f0ae7610b5e16b0b033
Ephraim Kigamba <ekigamba@ona.io>
2018-10-29 14:39:40 +0300

    Turn on WIFI to get MAC address
    - This fixes the issue where the MAC address is not available on non-SIM card devices when the Wifi (>=Android M devices) by turning it on, retrieving the Wifi MAC Address and restoring the Wifi state. The unique device ID is saved in preferences one gotten and this is what is retrieved everytime the device unique ID is required.
    
    Fixes #122

--- src/main/java/com/onaio/steps/helper/Device.java
+++ src/main/java/com/onaio/steps/helper/Device.java
@@ -17,12 +17,19 @@
 package com.onaio.steps.helper;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.onaio.steps.exceptions.NoUniqueIdException;
 
 import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.Collections;
 import java.util.List;
 
@@ -30,15 +37,16 @@ import java.util.List;
  * Created by Jason Rogena - jrogena@ona.io on 29/09/2016.
  */
 public class Device {
+    private static final String TAG = Device.class.getName();
+
     /**
      * This method returns the device's IMEI or the WLAN MAC Address if the IMEI is not
-     * available (highly unlikely)
+     * available - where the device does not have a SIM card module and thus no IMEI
      *
-     * @param context   The context to be used to get the unique ID
-     * @return  A unique device ID that is either the IMEI for the device or the WLAN MAC Address
-     * @throws NoUniqueIdException If a unique ID could not be gotten
+     * @return
      */
-    public static final String getUniqueDeviceId(Context context) throws NoUniqueIdException{
+    @Nullable @WorkerThread
+    public static final String generateUniqueDeviceId(Context context) {
         String uniqueId = null;
         if(context != null) {
             //try getting the IMEI
@@ -47,9 +55,51 @@ public class Device {
 
             //try getting the WLAN MAC address
             if(uniqueId == null) {
+                WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                boolean wasWifiEnabled = wifiManager.isWifiEnabled();
+
+                if (!wifiManager.isWifiEnabled()) {
+                    // ENABLE THE WIFI FIRST
+                    wifiManager.setWifiEnabled(true);
+                }
+
+                try {
+                    // Let's give the device some-time to start the wifi
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, Log.getStackTraceString(e));
+                }
+
                 uniqueId = getMacAddress();
+
+                if (!wasWifiEnabled) {
+                    wifiManager.setWifiEnabled(false);
+                }
             }
         }
+
+        return uniqueId;
+    }
+
+    public static final void saveUniqueDeviceId(Context context, @NonNull String deviceId) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(Constants.UNIQUE_DEVICE_ID, deviceId);
+        editor.commit();
+    }
+
+    /**
+     * Returns the saved device unique ID which is either the device's IMEI or WLAN MAC Address
+     * which was gotten from {@link #generateUniqueDeviceId(Context)} (Context)}
+     *
+     * @param context   The context to be used to get the unique ID
+     * @return  A unique device ID that is either the IMEI for the device or the WLAN MAC Address
+     * @throws NoUniqueIdException If a unique ID could not be gotten
+     */
+    public static final String getUniqueDeviceId(Context context) throws NoUniqueIdException{
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String uniqueId = sharedPreferences.getString(Constants.UNIQUE_DEVICE_ID, null);
 
         if(uniqueId == null) {
             throw new NoUniqueIdException();
@@ -83,7 +133,8 @@ public class Device {
                 }
                 return res1.toString();
             }
-        } catch (Exception ex) {
+        } catch (SocketException ex) {
+            Log.e(TAG, Log.getStackTraceString(ex));
         }
         return null;
     }
---
+++ src/main/java/com/onaio/steps/helper/RegisterUniqueDeviceIdTask.java
@@ -0,0 +1,35 @@
+package com.onaio.steps.helper;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.onaio.steps.exceptions.NoUniqueIdException;
+
+/**
+ * Created by Ephraim Kigamba - ekigamba@ona.io on 29/10/2018
+ */
+
+public class RegisterUniqueDeviceIdTask extends AsyncTask<Void, Void, Void> {
+
+    private Context context;
+
+    public RegisterUniqueDeviceIdTask(@NonNull Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            Device.getUniqueDeviceId(context);
+        } catch (NoUniqueIdException e) {
+            Log.e(RegisterUniqueDeviceIdTask.class.getName(), Log.getStackTraceString(e));
+
+            String uniqueDeviceId = Device.generateUniqueDeviceId(context);
+            Device.saveUniqueDeviceId(context, uniqueDeviceId);
+        }
+
+        return null;
+    }
+}
