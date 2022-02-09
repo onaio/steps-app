f517dbea8c1a8a2643a3d66d2a22c47b7c3245a3
owais-vd <owais.ali@venturedive.com>
2022-02-09 13:26:03 +0500

    added test case in ODKBlankFormTest class

--- src/test/java/com/onaio/steps/model/ODKForm/ODKBlankFormTest.java
+++ src/test/java/com/onaio/steps/model/ODKForm/ODKBlankFormTest.java
@@ -16,9 +16,25 @@
 
 package com.onaio.steps.model.ODKForm;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.nullable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.onaio.steps.StepsTestRunner;
+import com.onaio.steps.exceptions.AppNotInstalledException;
+import com.onaio.steps.exceptions.FormNotPresentException;
 import com.onaio.steps.model.ShadowDatabaseHelper;
 
 import org.junit.Assert;
@@ -38,7 +54,7 @@ public class ODKBlankFormTest extends StepsTestRunner {
 
     @Test
     public void ShouldGetTheFormURIWithId(){
-        Assert.assertEquals("content://org.odk.collect.android.provider.odk.forms/forms/id",form.getUri().toString());
+        assertEquals("content://org.odk.collect.android.provider.odk.forms/forms/id",form.getUri().toString());
     }
 
     @Test
@@ -49,4 +65,34 @@ public class ODKBlankFormTest extends StepsTestRunner {
         assertNotEquals(form.getUri().toString(), uriWithoutID);
     }
 
+    @Test
+    public void testFindShouldReturnOneEmptyForm() throws RemoteException, FormNotPresentException, AppNotInstalledException {
+        AppCompatActivity activity = mock(AppCompatActivity.class);
+        ContentResolver contentResolver = mock(ContentResolver.class);
+        ContentProviderClient contentProviderClient = mock(ContentProviderClient.class);
+        Cursor cursor = mock(Cursor.class);
+
+        when(activity.getContentResolver()).thenReturn(contentResolver);
+        when(contentResolver.acquireContentProviderClient(any(Uri.class))).thenReturn(contentProviderClient);
+        when(contentProviderClient.query(any(Uri.class), nullable(String[].class), anyString(), any(String[].class), nullable(String.class))).thenReturn(cursor);
+        when(cursor.moveToFirst()).thenReturn(true);
+        when(cursor.getColumnIndex("_id")).thenReturn(0);
+        when(cursor.getColumnIndex("jrFormId")).thenReturn(1);
+        when(cursor.getColumnIndex("displayName")).thenReturn(2);
+        when(cursor.getColumnIndex("jrVersion")).thenReturn(3);
+        when(cursor.getColumnIndex("formMediaPath")).thenReturn(4);
+        when(cursor.getString(0)).thenReturn(form._id);
+        when(cursor.getString(1)).thenReturn(form.jrFormId);
+        when(cursor.getString(2)).thenReturn(form.displayName);
+        when(cursor.getString(3)).thenReturn(form.jrVersion);
+        when(cursor.getString(4)).thenReturn(form.formMediaPath);
+
+        ODKBlankForm odkBlankForm = (ODKBlankForm) ODKBlankForm.find(activity, "");
+
+        assertEquals(form._id, odkBlankForm._id);
+        assertEquals(form.jrFormId, odkBlankForm.jrFormId);
+        assertEquals(form.displayName, odkBlankForm.displayName);
+        assertEquals(form.jrVersion, odkBlankForm.jrVersion);
+        assertEquals(form.formMediaPath, odkBlankForm.formMediaPath);
+    }
 }
