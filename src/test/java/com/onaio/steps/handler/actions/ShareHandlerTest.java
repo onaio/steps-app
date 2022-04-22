58a4a5d193b2f3709d3b8172ab016c2b191df9b4
owais <owais.ali@venturedive.com>
2022-04-22 19:40:11 +0500

    bump ShareHandlerTest coverage

--- src/test/java/com/onaio/steps/handler/actions/ShareHandlerTest.java
+++ src/test/java/com/onaio/steps/handler/actions/ShareHandlerTest.java
@@ -16,8 +16,19 @@
 
 package com.onaio.steps.handler.actions;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.onaio.steps.R;
 import com.onaio.steps.StepsTestRunner;
@@ -25,20 +36,22 @@ import com.onaio.steps.shadows.TestSettingsImportExportActivity;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.robolectric.Robolectric;
 
 public class ShareHandlerTest extends StepsTestRunner {
 
-    private TestSettingsImportExportActivity settingsImportExportActivity;
+    private TestSettingsImportExportActivity activity;
     private ShareHandler shareHandler;
+    private static final int MENU_ID = R.id.menu_item_settings_share;
 
     @Before
     public void setUp() {
-        settingsImportExportActivity = Robolectric.buildActivity(TestSettingsImportExportActivity.class)
+        activity = spy(Robolectric.buildActivity(TestSettingsImportExportActivity.class)
                 .create()
                 .visible()
-                .get();
-        shareHandler = new ShareHandler(settingsImportExportActivity, false);
+                .get());
+        shareHandler = new ShareHandler(activity, false);
     }
 
     @Test
@@ -51,9 +64,33 @@ public class ShareHandlerTest extends StepsTestRunner {
         assertFalse(shareHandler.shouldOpen(R.id.importCodeImageBtn));
     }
 
-
     @Test
     public void shouldDeactivate() {
         assertTrue(shareHandler.shouldDeactivate());
+    }
+
+    @Test
+    public void testActivateShouldVisibleAndEnableMenuItem() {
+        Menu menu = mock(Menu.class);
+        MenuItem menuItem = mock(MenuItem.class);
+
+        when(menu.findItem(MENU_ID)).thenReturn(menuItem);
+
+        shareHandler.withMenu(menu);
+        shareHandler.activate();
+
+        verify(menuItem, times(1)).setVisible(eq(true));
+        verify(menuItem, times(1)).setEnabled(eq(true));
+    }
+
+    @Test
+    public void testOnSuccessfulSaveShouldStartSharingIntent() {
+        shareHandler.onSuccessfulSave();
+
+        ArgumentCaptor<Intent> intentArgumentCaptor = ArgumentCaptor.forClass(Intent.class);
+        verify(activity, times(1)).startActivity(intentArgumentCaptor.capture());
+
+        Intent intent = intentArgumentCaptor.getValue();
+        assertEquals("android.intent.action.CHOOSER", intent.getAction());
     }
 }
