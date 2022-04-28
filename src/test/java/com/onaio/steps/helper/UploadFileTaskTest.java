30e24f12513b3219ae8226d1d66c7babae33fd14
owais <owais.ali@venturedive.com>
2022-04-28 18:41:10 +0500

    now when the upload gets failed then the dialog will show a specific error in order to make it clear

--- src/test/java/com/onaio/steps/helper/UploadFileTaskTest.java
+++ src/test/java/com/onaio/steps/helper/UploadFileTaskTest.java
@@ -1,8 +1,11 @@
 package com.onaio.steps.helper;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.onaio.steps.StepsTestRunner;
 import com.onaio.steps.activities.HouseholdListActivity;
@@ -11,6 +14,10 @@ import com.onaio.steps.handler.actions.ExportHandler;
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.Robolectric;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
 
 public class UploadFileTaskTest extends StepsTestRunner {
 
@@ -39,5 +46,24 @@ public class UploadFileTaskTest extends StepsTestRunner {
         assertFalse(uploadFileTask.isAllFieldValid(null, null));
         assertFalse(uploadFileTask.isAllFieldValid(""));
         assertFalse(uploadFileTask.isAllFieldValid("", ""));
+    }
+
+    @Test
+    public void testFindErrorShouldReturnNullOnWhenErrorBodyIsNull() {
+        assertNull(uploadFileTask.findError(null));
+    }
+
+    @Test
+    public void testFindErrorShouldReturnNullWhenExceptionOccur() throws IOException {
+        ResponseBody errorBody = mock(ResponseBody.class);
+        when(errorBody.string()).thenReturn("");
+        assertNull(uploadFileTask.findError(errorBody));
+    }
+
+    @Test
+    public void testFindErrorShouldReturnValidError() throws IOException {
+        ResponseBody errorBody = mock(ResponseBody.class);
+        when(errorBody.string()).thenReturn("{\"error\": \"invalid username or password\"}");
+        assertEquals("invalid username or password", uploadFileTask.findError(errorBody));
     }
 }
